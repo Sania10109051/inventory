8e81e1198cb42b15e8bf3e2b88d3582af41e6be8
Sania Nurul Fitria <saniafitria12@gmail.com>
2024-11-24 22:31:52 +0700

    test commit

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -10,7 +10,7 @@
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">List Kategori</h5>
-                <table class="table table-striped">
+                {{-- <table class="table table-striped">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -38,7 +38,42 @@
                     </tbody>
                 </table>
             </div>
-        </div>
+        </div> --}}
+        <table class="table table-data cell-border text-center hover">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Kategori Barang</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($kategori as $item)
+                    <tr>
+                        <th scope="row">{{ $loop->iteration }}</th>
+                        <td>{{ $item->nama_kategori }}</td>
+                        <td>
+                            <!-- Edit Button -->
+                            <a href="{{ route('kategori.edit', $item->id_kategori) }}">
+                                <button type="button" class="btn btn-warning btn-sm">
+                                    <i class="ri ri-pencil-line"></i> 
+                                </button>
+                            </a>
+                            
+                            <!-- Delete Button -->
+                            <form action="{{ route('kategori.delete', $item->id_kategori) }}" method="post" class="d-inline">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger btn-sm" onclick="confirmModal(event)">
+                                    <i class="ri ri-delete-bin-5-line"></i> 
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>        
         <x-confirm-modal></x-confirm-modal>
     </x-section>
 </x-layout>
